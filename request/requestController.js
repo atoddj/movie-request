638f24d802a98690bff365831b4772c703032a14
Todd Jones <anthony.jones@cgifederal.com>
2018-05-21 10:08:12 -0400

    Renamed variable for clarity

--- request/requestController.js
+++ request/requestController.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var request = require('request');
+var fetch = require('request');
 var Request = require('./request');
 var config = require('../config/config');
 var Slack = require('node-slackr');
@@ -23,10 +23,10 @@ router.post('/', (req, res) => {
 
 // List requests
 router.get('/', (req, res) => {
-  Request.find({status: 'pending'}, (err, result) => {
+  Request.find({}, (err, result) => {
     if (err) return console.log(err);
     if (req.query.q) {
-      request({
+      fetch({
         uri: config.tmdb.movieSearch,
         qs: {
           api_key: config.tmdb.api_key,
@@ -36,7 +36,6 @@ router.get('/', (req, res) => {
         body = JSON.parse(body);
         if(err) console.log(err);
         req.query.results = body;
-        console.log(body);
         res.render('request.ejs', {movies: result, searchQuery: req.query});
       });
     } else {
