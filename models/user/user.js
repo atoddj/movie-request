64dc8ac5e11a83d26bc3c125fb4a8e311c9765f9
Todd Jones <atoddj@gmail.com>
2018-05-21 13:23:12 -0400

    Updated spacing

--- models/user/user.js
+++ models/user/user.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 var userSchema = new mongoose.Schema({
-  username: {type: String, required: true},
-  email: {type: String, required: true},
+  username: {type: String, required: true, unique: true},
+  email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
   role: {type: String, required: true, default: 'user'}
-  },
-  {collection: 'users'}
-);
+},{
+  collection: 'users'
+});
 mongoose.model('User', userSchema);
 module.exports = mongoose.model('User');
