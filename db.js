03d98860db217f6d5be6b01887bf4abb773e1826
Todd Jones <atoddj@gmail.com>
2019-11-19 13:25:40 -0500

    Wiped branch

--- db.js
+++
@@ -1,3 +0,0 @@
-var mongoose = require('mongoose');
-var config = require('./config/config');
-mongoose.connect(config.mongodb.uri);
--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = require('./app');
-var config = require('./config/config');
-var port = process.env.PORT || 8000;
-
-var server = app.listen(port, () => {
-  console.log('listening on port ' + port);
-});
