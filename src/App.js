221be5503acedb1c7065be3cf73752ac2e1563b8
Todd Jones <atoddj@gmail.com>
2019-11-20 16:22:11 -0500

    Added Search component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import RequestList from './RequestList';
+import Search from './Search';
 import './App.css';
 
 function App() {
@@ -7,6 +8,8 @@ function App() {
     <div className="App">
       <h1>Pending requests</h1>
       <RequestList />
+      <h2>Request a friggin movie</h2>
+      <Search />
     </div>
   );
 }
