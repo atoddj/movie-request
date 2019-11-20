15cabd54c3b6541ddc6559c9bd66ba8ca799eefa
Todd Jones <atoddj@gmail.com>
2019-11-20 16:22:54 -0500

    Added searchform and result child components

---
+++ src/SearchForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class SearchForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = { query: '' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        const {query} = this.state;
+        e.preventDefault();
+        this.props.performSearch(query);
+    }
+
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render() {
+        const {query} = this.state;
+        return ( 
+            <div className="SearchForm">
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="query-input"></label>
+                    <input type="text" name="query" id="query-input" onChange={this.handleChange} value={query} />
+                    <button>Search</button>
+                </form>
+            </div>
+         )
+    }
+}
+ 
+export default SearchForm;
