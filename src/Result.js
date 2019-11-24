2fdb8708c4ea2d8bbe55820b5cecdfd15afdd979
Todd Jones <atoddj@gmail.com>
2019-11-23 23:20:38 -0500

    adding elements for results

--- src/Result.js
+++ src/Result.js
@@ -6,7 +6,14 @@ class Result extends Component {
         const {result} = this.props;
         return ( 
             <div className="Result">
-                {result.original_name || result.original_title}
+                <div className="media-card">
+                    <div className="media-header">
+                        <img src={`//image.tmdb.org/t/p/w92/${result.poster_path}`} alt={`Poster for ${result.original_name || result.original_title}`} />
+                        <h2>{result.original_name || result.original_title}</h2>
+                    </div>
+                </div>
+                
+                
             </div>
         );
     }
