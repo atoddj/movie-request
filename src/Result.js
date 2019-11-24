fa3c47489479f2be9a3ceb8ba75a6b6a2150d2bd
Todd Jones <atoddj@gmail.com>
2019-11-24 11:59:17 -0500

    Updated search result format

--- src/Result.js
+++ src/Result.js
@@ -5,15 +5,15 @@ class Result extends Component {
     render() {
         const {result} = this.props;
         const relDate = result.release_date ? new Date(result.release_date).getFullYear() : null;
+        const firstAirDate = result.first_air_date ? new Date(result.first_air_date).getFullYear() : null;
         return ( 
             <div className="Result">
                 <div className="media-card">
                     <div className="media-header">
                         <img src={`//image.tmdb.org/t/p/w92/${result.poster_path}`} alt={`Poster for ${result.original_name || result.original_title}`} />
                         <h2>{result.original_name || result.original_title}</h2>
-                        {relDate && 
-                            <span>{relDate}</span>
-                        }
+                        {result.media_type && <span>{result.media_type}</span>}
+                        {(relDate||firstAirDate) && <span>{relDate||firstAirDate}</span>}
                     </div>
                     <div className="media-desc">
                         <p>{result.overview}</p>
