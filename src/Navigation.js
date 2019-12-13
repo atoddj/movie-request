f61ec8ff7b23619cc76e6e954c7e8472d28d6f49
Todd Jones <atoddj@gmail.com>
2019-12-13 16:36:49 -0500

    Worked on responsive menu

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,40 +1,45 @@
-import React, { Component } from 'react';
-import SearchForm from './SearchForm';
-import './Navigation.css';
+import React, { Component } from "react";
+import SearchForm from "./SearchForm";
+import "./Navigation.css";
 
 class Navigation extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  };
-        this.handleTabClick = this.handleTabClick.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleTabClick = this.handleTabClick.bind(this);
+  }
 
-    handleTabClick(e) {
-        const {handleClick} = this.props;
-        handleClick(e);
-    }
+  handleTabClick(e) {
+    const { handleClick } = this.props;
+    handleClick(e);
+  }
 
-    render() {
-        const {tabs, activeTab} = this.props;
-        const tabList = tabs.map(t => {
-            return(
-                <li className={activeTab === t.name ? 'active' : ''} name={t.name} onClick={this.handleTabClick} >
-                    {t.name}
-                </li>
-            )
-        });
-        return (
-            <nav className="navbar">
-                <a href="/" className="logo">
-                    Floobie
-                </a>
-                <ul className="tab-list">
-                    {tabList}
-                    <SearchForm performSearch={this.props.performSearch} />
-                </ul>
-            </nav>
-        )
-    }
+  render() {
+    const { tabs, activeTab } = this.props;
+    const tabList = tabs.map(t => {
+      return (
+        <li
+          className={activeTab === t.name ? "item current" : "item"}
+          name={t.name}
+          onClick={this.handleTabClick}
+        >
+          {t.name}
+        </li>
+      );
+    });
+    return (
+      <nav>
+        <ul className="menu">
+          <li className="logo">Floobie</li>
+          {tabList}
+          <li className="toggle">
+            <i className="fas fa-bars" />
+          </li>
+          <SearchForm performSearch={this.props.performSearch} />
+        </ul>
+      </nav>
+    );
+  }
 }
- 
+
 export default Navigation;
