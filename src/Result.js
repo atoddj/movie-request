047ccdca7e77fc7da652841363c291e86d116cb5
Todd Jones <atoddj@gmail.com>
2019-12-15 21:31:17 -0500

    Started work for tv show modal

--- src/Result.js
+++ src/Result.js
@@ -1,59 +1,103 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Result extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isModalOpen: false, showObject: {} };
 
-    constructor(props) {
-        super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.openModal = this.openModal.bind(this);
+  }
 
-        this.handleClick = this.handleClick.bind(this);
+  handleClick() {
+    const { postNewRequest, result } = this.props;
+    postNewRequest(result);
+  }
+
+  openModal() {
+    //perform new api call to show endpoint
+    //set open modal state
+    this.setState({ isModalOpen: true, showObject: { title: String, numSeasonsAvailable: Array, numSeasonsReady: Array } });
+  }
+
+  render() {
+    const { result } = this.props;
+    const { isModalOpen, showObject } = this.state;
+    const relDate = result.release_date
+      ? new Date(result.release_date).getFullYear()
+      : null;
+    const firstAirDate = result.first_air_date
+      ? new Date(result.first_air_date).getFullYear()
+      : null;
+    const isPending = result.status === "pending";
+    const isAvailable = result.status === "Available";
+    const seasons = result.seasons
+      ? result.seasons.map(s => <span className="Result-season">{s}</span>)
+      : "";
+
+    const isTvShow = result.media_type === "tv";
+
+    let button;
+
+    if (!isAvailable && !isPending) {
+      button = (
+        <button className="Result-button" onClick={this.handleClick}>
+          Request
+        </button>
+      );
+    } else if (isPending) {
+      button = (
+        <div className="Result-pending">
+          <i className="fas fa-exclamation-triangle"></i> Request Pending
+        </div>
+      );
+    } else if (isAvailable) {
+      button = (
+        <div className="Result-available">
+          <i className="fas fa-check-circle"></i> Available for streaming
+        </div>
+      );
     }
 
-    handleClick() {
-        const {postNewRequest, result} = this.props;
-        postNewRequest(result);
+    if (isTvShow) {
+      // update button to new button to open a modal if it's a tv show
+      button = (
+        <button className="Result-button" onClick={this.openModal}>
+          Request...
+        </button>
+      );
     }
 
-    render() {
-        const {result} = this.props;
-        const relDate = result.release_date ? new Date(result.release_date).getFullYear() : null;
-        const firstAirDate = result.first_air_date ? new Date(result.first_air_date).getFullYear() : null;
-        const isPending = result.status === "pending";
-        const isAvailable = result.status === 'Available';
-        const seasons = result.seasons ? result.seasons.map(s => (<span className="Result-season">{s}</span>)) : '';
-        return ( 
-            <div className="Result">
-                <div className="media-card">
-                    <div className="media-header">
-                        <img src={`//image.tmdb.org/t/p/w92/${result.poster_path}`} alt={`Poster for ${result.name || result.title}`} />
-                        <h2>{result.name || result.title}</h2>
-                        {result.media_type && <span>{result.media_type},</span>}
-                        {(relDate||firstAirDate) && <span> {relDate||firstAirDate}</span>}
-                        {seasons && 
-                            <div className="Result-seasons">
-                                {seasons}
-                            </div>
-                        }
-                    </div>
-                    <div className="media-desc">
-                        <p>{result.overview}</p>
-                    </div>
-                    <div className="media-card-button">
-                        {(!isAvailable && !isPending) && 
-                            <button className="Result-button" disabled={isPending} onClick={this.handleClick} >{isPending ? 'Pending' : 'Request'}</button>
-                        }
-                        {isPending && 
-                            <div className="Result-pending"><i className="fas fa-exclamation-triangle"></i> Request Pending</div>
-                        }
-                        {isAvailable &&
-                            <div className="Result-available"><i className="fas fa-check-circle"></i> Available for streaming</div>
-                            
-                        }
-                    </div>
-                </div>
-                <div className="blur-back" style={{backgroundImage: `url('//image.tmdb.org/t/p/original${result.backdrop_path}')`}}></div>
-            </div>
-        );
-    }
+    return (
+      <div className="Result">
+        <div className="media-card">
+          <div className="media-header">
+            <img
+              src={`//image.tmdb.org/t/p/w92/${result.poster_path}`}
+              alt={`Poster for ${result.name || result.title}`}
+            />
+            <h2>{result.name || result.title}</h2>
+            {result.media_type && <span>{result.media_type},</span>}
+            {(relDate || firstAirDate) && (
+              <span> {relDate || firstAirDate}</span>
+            )}
+            {seasons && <div className="Result-seasons">{seasons}</div>}
+          </div>
+          <div className="media-desc">
+            <p>{result.overview}</p>
+          </div>
+          <div className="media-card-button">{button}</div>
+        </div>
+        <div
+          className="blur-back"
+          style={{
+            backgroundImage: `url('//image.tmdb.org/t/p/original${result.backdrop_path}')`
+          }}
+        ></div>
+        {isModalOpen && <div className="Modal"></div>}
+      </div>
+    );
+  }
 }
- 
+
 export default Result;
